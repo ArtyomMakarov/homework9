3d506b1afda823b6080faf537012604aeb471135
Arsem Makarov <Makar6031994@gmail.com>
2019-05-23 16:08:38 +0300

    fixed

--- task4-JSlibrary/js/main.js
+++ task4-JSlibrary/js/main.js
@@ -1,6 +1,11 @@
 window.onload = function () {
-    console.log(library.attr('test', 'testing value').html());
-    library.actions('click', function () {
+    let library = new Library('div');
+    let block = document.getElementById('block');
+    console.log(library.attr('type', 'testing value').html());
+    block.addEventListener('click', function () {
         return library.changeBackgroundColor();
     });
+    block.addEventListener('dblclick', function () {
+        return library.hide();
+    })
 };
